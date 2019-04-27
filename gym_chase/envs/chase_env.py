85d0f80099e0dc35a39826ca3c37d91533013b2c
natalia navas <natinavas@gmail.com>
2019-04-27 16:58:32 -0300

    Add dict that will map matrices to state numbers so it will be compatible with our implementation of the Q Table algorithm

--- gym_chase/envs/chase_env.py
+++ gym_chase/envs/chase_env.py
@@ -3,6 +3,29 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 
+def get_state_map(num_rows_cols):
+    state_map = dict()
+    state_number = 0
+    for i in range(num_rows_cols):
+        for j in range(num_rows_cols):
+            base_matrix = [['-' for _ in range(num_rows_cols)] for _ in range(num_rows_cols)]
+            base_matrix[i][j] = 'X'
+            state_map[get_matrix_string(base_matrix)] = state_number
+            state_number += 1
+    return state_map
+
+
+def get_matrix_string(matrix):
+    matrix_string = ""
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if matrix[i][j] == "G":
+                matrix_string += "-,"
+            else:
+                matrix_string += matrix[i][j] + ","
+    return matrix_string
+
+
 class ChaseEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
@@ -10,12 +33,13 @@ class ChaseEnv(gym.Env):
         self.reached_goal = False
         self.state = [['-' for _ in range(num_row_cols)] for _ in range(num_row_cols)]
         self.state[initial_pos[0]][initial_pos[1]] = "X"
-        self.num_row_cols = num_row_cols
+        self.num_rows_cols = num_row_cols
         self.current_position = initial_pos
         self.goal = goal
         self.state[goal[0]][goal[1]] = "G"
         self.number_of_steps = 0
         self.initial_position = initial_pos
+        self.state_map = get_state_map(num_row_cols)
 
     def step(self, action):
         reward = 0
@@ -23,7 +47,7 @@ class ChaseEnv(gym.Env):
         if action == 0:  # Upwards Direction
             if self.current_position[0] == 0:
                 print("Invalid Step")
-                return self.state, reward, self.reached_goal, self.number_of_steps
+                return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
 
             self.state[self.current_position[0]][self.current_position[1]] = "-"
             self.state[self.current_position[0] - 1][self.current_position[1]] = "X"
@@ -32,12 +56,12 @@ class ChaseEnv(gym.Env):
             if self.current_position == self.goal:
                 print("You reached your goal!")
                 reward = 1  # TODO ver tema del reward
-                return self.state, reward, self.reached_goal, self.number_of_steps
+                return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
 
         elif action == 1:  # Right Direction
-            if self.current_position[1] == self.num_row_cols - 1:
+            if self.current_position[1] == self.num_rows_cols - 1:
                 print("Invalid Step")
-                return self.state, reward, self.reached_goal, self.number_of_steps
+                return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
 
             self.state[self.current_position[0]][self.current_position[1]] = "-"
             self.state[self.current_position[0]][self.current_position[1] + 1] = "X"
@@ -46,10 +70,10 @@ class ChaseEnv(gym.Env):
             if self.current_position == self.goal:
                 print("You reached your goal!")
                 reward = 1  # TODO ver tema del reward
-                return self.state, reward, self.reached_goal, self.number_of_steps
+                return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
 
         elif action == 2:  # Downwards Direction
-            if self.current_position[0] == self.num_row_cols - 1:
+            if self.current_position[0] == self.num_rows_cols - 1:
                 print("Invalid Step")
                 return self.state, reward, self.reached_goal, self.number_of_steps
             self.state[self.current_position[0]][self.current_position[1]] = "-"
@@ -59,12 +83,12 @@ class ChaseEnv(gym.Env):
             if self.current_position == self.goal:
                 print("You reached your goal!")
                 reward = 1  # TODO ver tema del reward
-                return self.state, reward, self.reached_goal, self.number_of_steps
+                return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
 
         elif action == 3:  # Left Direction
             if self.current_position[1] == 0:
                 print("Invalid Step")
-                return self.state, reward, self.reached_goal, self.number_of_steps
+                return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
 
             self.state[self.current_position[0]][self.current_position[1]] = "-"
             self.state[self.current_position[0]][self.current_position[1] - 1] = "X"
@@ -73,30 +97,33 @@ class ChaseEnv(gym.Env):
             if self.current_position == self.goal:
                 print("You reached your goal!")
                 reward = 1  # TODO ver tema del reward
-                return self.state, reward, self.reached_goal, self.number_of_steps
+                return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
         else:
             raise Exception("Action is not valid since it is not in the action space")
-        return self.state, reward, self.reached_goal, self.number_of_steps
+        return self.state_map[get_matrix_string(self.state)], reward, self.reached_goal, self.number_of_steps
 
     def reset(self):
-        self.state = [['-' for _ in range(self.num_row_cols)] for _ in range(self.num_row_cols)]
+        self.state = [['-' for _ in range(self.num_rows_cols)] for _ in range(self.num_rows_cols)]
         self.state[self.initial_position[0]][self.initial_position[1]] = "X"
         self.state[self.goal[0]][self.goal[1]] = "G"
 
     def render(self, mode='human', close=False):
-        for i in range(self.num_row_cols):
-            for j in range(self.num_row_cols):
+        for i in range(self.num_rows_cols):
+            for j in range(self.num_rows_cols):
                 print(self.state[i][j] + " ", end = '')
             print()
 
 
 chase_env = ChaseEnv()
 chase_env.render()
-chase_env.step(2)
-chase_env.step(2)
-chase_env.step(1)
-chase_env.step(1)
-chase_env.step(1)
+print(chase_env.step(2))
+chase_env.render()
+print(chase_env.step(2))
+chase_env.render()
+print(chase_env.step(1))
+chase_env.render()
+print(chase_env.step(1))
+print(chase_env.step(1))
 
 chase_env.render()
 
