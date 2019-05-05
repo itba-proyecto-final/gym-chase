7314bec38be6da4e0428c0fbe97d68a565b90e79
josevitali <jvitali@itba.edu.ar>
2019-05-05 16:39:42 -0300

    fix setup file

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_chase',
       version='0.0.1',
+      packages=['gym_chase', ],
       install_requires=['gym']  # And any other dependencies required
 )
