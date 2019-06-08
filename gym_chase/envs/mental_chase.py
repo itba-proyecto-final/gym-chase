612daedab59fbd87fe345126f9810c7f8d5a2d51
natalia navas <natinavas@gmail.com>
2019-06-08 17:32:14 -0300

    Add integer that represents the possible actions and add docstrings

--- gym_chase/envs/mental_chase.py
+++ gym_chase/envs/mental_chase.py
@@ -1,40 +1,54 @@
 import gym
 from math import floor
 
+
 POSITION = 0
 GOAL = 1
 REWARD = 2
 
 __END_OF_EXPERIENCE = "--\n"
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 
 def get_action_map():
     action_map = dict()
-    action_map[0] = "Up"
-    action_map[1] = "Right"
-    action_map[2] = "Down"
-    action_map[3] = "Left"
+    action_map[UP] = "Up"
+    action_map[RIGHT] = "Right"
+    action_map[DOWN] = "Down"
+    action_map[LEFT] = "Left"
     return action_map
 
 
 def read_game_file(filename):
+    """
+    Read a file containing the state and return amount of rows and cols and a list with information on each experience
+    :param filename: File from where the game is read, complete path
+    :return: amount of rows, amount of cols and a list of experiences where each experience is identified by three lists
+    one containing all positions, another containing all goals and the last one containing all rewards, all three lists
+    should have the same size, since at the same position all three should represent the state of the game at one given
+    moment
+    """
     file = open(filename, "r")
     lines = file.readlines()
     [rows, cols] = lines[0].replace("\n", "").split("x")
     experiences = list()
-    experience = (list(), list(), list())
-    for l in lines[1:]:
-        if l != "\n":
-            if '-' in l and l != __END_OF_EXPERIENCE and not l.startswith("-"):
-                [current_position, goal_position] = l.replace("\n", "").split("-")
+    experience = (list(), list(), list())  # position, goal, reward
+    for line in lines[1:]:
+        if line != "\n":
+            if '-' in line and line != __END_OF_EXPERIENCE and not line.startswith("-"):
+                [current_position, goal_position] = line.replace("\n", "").split("-")
                 experience[POSITION].append(int(current_position))
                 experience[GOAL].append(int(goal_position))
-            elif l == __END_OF_EXPERIENCE:
+            elif line == __END_OF_EXPERIENCE:
                 check_experience_validity(experience)
                 experiences.append(experience)
                 experience = (list(), list(), list())
             else:
-                experience[REWARD].append(int(l.replace("\n", "")))
+                experience[REWARD].append(int(line.replace("\n", "")))
     file.close()
     return int(rows), int(cols), experiences
 
@@ -76,25 +90,43 @@ class MentalChaseEnv(gym.Env):
         self.already_reset = False
 
     def get_row_col_from_state(self, state):
+        """
+        From state represented in a matrix it returns a number which represents the state
+        :param state: Matrix which represents state
+        :return: Numerical value of state
+        """
         row = floor(state / self.cols)
         col = state - row * self.cols
         return row, col
 
     def calculate_reward(self):
+        """
+        Get reward by popping the list of experiences that was read from the file and that contains the rewards
+        :return: integer representing reward
+        """
         return self.experiences[0][REWARD].pop(0)
 
-    # TODO revisar porque no contempla casos bordes pero tampoco es necesario porque si se hizo una accion correcta no debería pasar nada bizarro
     def get_action(self):
+        """
+        Get number that represents the action that has taken place
+        :return: an integer representing the action that was taken, where 0 represents up, 1 represents right,
+        2 represents down and 3 represents left.
+        """
         if self.state == self.previous_state + 1:  # Right
-            return 1
+            return RIGHT
         elif self.state == self.previous_state - 1:  # Left
-            return 3
+            return LEFT
         elif self.state < self.previous_state:  # Up
-            return 0
+            return UP
         else:  # Down
-            return 2
+            return DOWN
 
     def step(self):
+        """
+        Advance game to the next step, it does this by reading the game file and getting next position, reward and goal
+        :return: new state, reward for this step, boolean indicating if the goal was reached and action that was
+        executed
+        """
         self.number_of_steps += 1
         self.previous_state = self.state
         if len(self.experiences[0][POSITION]) != 0:
@@ -105,6 +137,11 @@ class MentalChaseEnv(gym.Env):
         return self.state, self.calculate_reward(), self.reached_goal, self.get_action()
 
     def reset(self):
+        """
+        Reset environment by removing the current experience from the list of experiences, reset the number of steps,
+        set the state back to the initial state, and set boolean indicating if the goal was reached back to false
+        :return:
+        """
         if self.already_reset:
             self.experiences.pop(0)
             self.initial_state = self.experiences[0][POSITION][0]
@@ -114,7 +151,11 @@ class MentalChaseEnv(gym.Env):
         self.already_reset = True
         return self.state
 
-    def render(self, mode='human', close=False):
+    def render(self):
+        """
+        Render game state, by printing the state in the console
+        :return:
+        """
         for i in range(self.rows):
             for j in range(self.cols):
                 if i * self.cols + j == self.state:
