fbc22fdf4712e3d5f486ce6a693852328f9fce8b
Unknown <jvitali@itba.edu.ar>
2019-11-24 16:21:05 -0300

    add gym_chase.envs to packages

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='gym_chase',
       version='0.0.1',
-      packages=['gym_chase', ],
+      packages=['gym_chase', 'gym_chase.envs'],
       install_requires=['gym']  # And any other dependencies required
 )
